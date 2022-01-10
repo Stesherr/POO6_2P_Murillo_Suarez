6210b8c14ee490b6bad0c5995bfb464eeb5c681f
Stesherr <stesherr@espol.edu.ec>
2022-01-10 12:37:17 -0500

    Se agrega el Proyecto

---
+++ src/main/java/com/pooespol/proyectopoo_2p_murillo_suarez/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.pooespol.proyectopoo_2p_murillo_suarez;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
