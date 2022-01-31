dc7d2a6c91e33a2d01b00fdc26a6f9b778e0030b
joismuri <joismuri@espol.edu.ec>
2022-01-30 19:09:24 -0500

    arreglo de pane

---
+++ src/main/resources/com/pooespol/proyectopoo_2p_murillo_suarez/ConsultarCitasController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pooespol.proyectopoo_2p_murillo_suarez;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+iimport javafx.fxml.FXML;
+mport javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+/**
+ * FXML Controller class
+ *
+ * @author josem
+ */
+public class ConsultarCitasController implements Initializable {
+
+
+    @FXML
+    private VBox root;
+    @FXML
+    private VBox nombreVb;
+    @FXML
+    private Button cerrar;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
